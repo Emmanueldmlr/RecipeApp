515133854e3cca211aa19b2fd3836b0b62d023a1
Damilare Bakare <bakaredmlr@gmail.com>
2024-01-08 14:57:59 +0000

    sets up routing

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import SplashScreen from './src/screens/Splash';
+import "react-native-gesture-handler";
+import SplashScreen from "./src/screens/Splash";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./src/screens/Home";
+import { useEffect } from "react";
+const Stack = createStackNavigator();
 
 export default function App() {
+
   return (
-    <SplashScreen/>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -1,30 +1,39 @@
 import React from 'react'
-import { View, Text, ImageBackground, Image } from "react-native";
+import { View, Text, ImageBackground, Image, SafeAreaView } from "react-native";
 import Button from '../../components/Button';
 import { splashStyle } from './styles';
 
-const SplashScreen = () => {
+const SplashScreen = ({navigation}) => {
   return (
-    <ImageBackground
-      source={require("../../../assets/splash.png")}
-      style={splashStyle.imageBackground}
-    >
-      <View style={splashStyle.container}>
-        <View style={splashStyle.header}>
-          <Image
-            style={splashStyle.icon}
-            source={require("../../../assets/logo.png")}
-          />
-          <Text style={splashStyle.text}>100k+ Premium Recipes</Text>
+    <SafeAreaView>
+      <ImageBackground
+        source={require("../../../assets/splash.png")}
+        style={splashStyle.imageBackground}
+      >
+        <View style={splashStyle.container}>
+          <View style={splashStyle.header}>
+            <Image
+              style={splashStyle.icon}
+              source={require("../../../assets/logo.png")}
+            />
+            <Text style={splashStyle.text}>100k+ Premium Recipes</Text>
+          </View>
+          <View style={splashStyle.footer}>
+            <Text style={splashStyle.footerHeaderText}>Get </Text>
+            <Text style={splashStyle.footerHeaderText}>Cooking</Text>
+            <Text style={splashStyle.footerText}>
+              Simple ways to find Tasty Recipe
+            </Text>
+            <Button
+              style={splashStyle.footerButton}
+              onPress={() => navigation.navigate("Home")}
+            >
+              Start Cooking
+            </Button>
+          </View>
         </View>
-        <View style={splashStyle.footer}>
-          <Text style={splashStyle.footerHeaderText}>Get </Text>
-          <Text style={splashStyle.footerHeaderText}>Cooking</Text>
-          <Text style={splashStyle.footerText}>Simple ways to find Tasty Recipe</Text>
-          <Button style={splashStyle.footerButton} onPress={() => alert("Hello World")}>Start Cooking</Button>
-        </View>
-      </View>
-    </ImageBackground>
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
